2aed8154c154b6316a66e71db5a939a7cb196b1f
mosquedakike <enrique_inf@hoptmail.com>
2019-10-09 13:17:22 -0500

    metodos publicos y privados

--- netCore_v2/Program.cs
+++ netCore_v2/Program.cs
@@ -4,9 +4,44 @@ namespace netCore_v2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var data = new Program();
+            var retornar = data.retornaIntToString();
+            
+            data.metodoPrivado();
+            data.metodoPublico();
+            data.retortoString();
+
+            Console.WriteLine(data.retortoString());
+            Console.WriteLine(retornar);
+
+            Console.ReadLine();
         }
+
+        //DECLARANDO METODOS PRIVADOS | NO RETORNA VALORES
+        private void metodoPrivado()
+        {
+            Console.WriteLine("Hola desde un metodo private!");
+        }
+
+        //DECLARANDO METODOS PUBLICOS | NO RETORNA VALORES
+        public void metodoPublico()
+        {
+            Console.WriteLine("Hola desde un metodo public!");
+        }
+
+        //DECLARANDO METODOS PRIVADOS | SI RETORNA VALORES | TIPO STRING
+        private string retortoString()
+        {
+            return "Soy una cadena con return";
+        }
+
+        //DECLARANDO METODOS PRIVADOS | SI RETORNA VALORES | TIPO INT TO STRING
+        private int retornaIntToString()
+        {
+            return Convert.ToInt16("16");
+        }
+        
     }
 }
