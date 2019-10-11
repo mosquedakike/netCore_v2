ae0826b9ff093fa01edd5a26ed4bfcac970824ec
mosquedakike <enrique_inf@hoptmail.com>
2019-10-11 09:19:13 -0500

    agregando funciones para identificar may y min

--- netCore_v2/Program.cs
+++ netCore_v2/Program.cs
@@ -4,9 +4,77 @@ namespace netCore_v2
 {
     class Program
     {
+        private string cadena;
+        private string verMayusculas = "";
+        private string verMinusculas = "";
+        private int n;
+        private string[] Mayusculas;
+        private string[] Minusculas;
+        private string[] tempMy;
+        private string[] tempMn;
+
+        public Program(string cadena)
+        {
+            this.cadena = cadena;
+            n = cadena.Length;
+            Mayusculas = new string[n];
+            Minusculas = new string[n];
+            tempMy = new string[n];
+            tempMn = new string[n];
+        }
+
+        private string mayusculas()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                //[IsUpper] sirve para identificar las letras mayusculas
+                if (Char.IsUpper(cadena[i]))
+                {
+                    tempMy[i] = Convert.ToString(cadena[i]);
+                }
+            }
+
+            for (int i = 0; i < tempMy.Length; i++)
+            {
+                if (tempMy[i] != null)
+                {
+                    Mayusculas[i] = tempMy[i];
+                    //verMayusculas = verMayusculas + "," + verMayusculas[i];
+                    verMayusculas = $"{verMayusculas},{Mayusculas[i]}";
+                }
+            }
+            return verMayusculas;
+        }
+
+        private string minusculas()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                //[IsUpper] sirve para identificar las letras minusculas
+                if (Char.IsLower(cadena[i]))
+                {
+                    tempMn[i] = Convert.ToString(cadena[i]);
+                }
+            }
+
+            for (int i = 0; i < tempMn.Length; i++)
+            {
+                if (tempMy[i] != null)
+                {
+                    Minusculas[i] = tempMn[i];
+                    //verMayusculas = verMayusculas + "," + verMayusculas[i];
+                    verMinusculas = $"{verMinusculas},{Minusculas[i]}";
+                }
+            }
+            return verMinusculas;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cadena = Console.ReadLine();
+            var data = new Program(cadena);
+
+            Console.ReadLine();
         }
     }
 }
