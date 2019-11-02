389e9697865f451954a9bb66d0a06d51a5517324
Quique <enrique_inf@hotmail.com>
2019-11-02 16:19:22 -0600

    PARAMETROS CON ARRAYS

--- netCore_v2/Program.cs
+++ netCore_v2/Program.cs
@@ -6,7 +6,18 @@ namespace netCore_v2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Object[] parametros = {"kike", 25, 10, "Gran desarrollador", true, 25.2 };
+            new Program().metodo(parametros);
+            Console.ReadLine();    
+        }
+
+        //SI NECESITAMOS PASAR VARIOS PARAMETROS A UN METODO, TENEMOS LA OPCION DE HACERLO CON UN ARRAYS
+        private void metodo(object[] parametros)
+        {
+            String nombre = (string)parametros[0];
+            int edad = (int)parametros[1];
+            String puesto = (string)parametros[3];
+            Console.WriteLine(nombre + " " + edad + " " + puesto);
         }
     }
 }
