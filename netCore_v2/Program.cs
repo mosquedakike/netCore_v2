b3d209d63e5d6c642b9cb99105fd5abc3339e847
Luis Enrique Hernandez <enrique_inf@hotmail.com>
2019-11-14 20:46:10 -0600

    funcions locales y expresion landa

--- netCore_v2/Program.cs
+++ netCore_v2/Program.cs
@@ -6,7 +6,20 @@ namespace netCore_v2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int data;
+            String nombre;
+            //new Program().metodo(out data);
+            mensaje();
+            Console.WriteLine(nombre);
+            void mensaje() => nombre = "Luis Enrique Hernandez Mosqueda";
+        }
+            private void metodo(out int valor)
+            {
+                valor = funcion();
+                //EXPRESION LANDA =>
+                static int funcion() => 50 + 20;
+
+            }
         }
     }
-}
+
